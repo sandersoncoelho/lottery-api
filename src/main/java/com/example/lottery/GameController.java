d537ccb563e7aab772ddf416f3793a13beb7be6e
Sanderson Coelho <sanderson.mc@gmail.com>
2022-01-25 23:17:38 -0300

    Initial commit

---
+++ src/main/java/com/example/lottery/GameController.java
@@ -0,0 +1,17 @@
+package com.example.lottery;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/game")
+public class GameController {
+
+    @GetMapping
+    public List<Integer> getGames() {
+        return List.of(4, 7, 17, 54, 23, 58);
+    }
+}
